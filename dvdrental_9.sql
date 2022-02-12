969d92f2412f6064dd4f8f11f016810956871ecb
Mert Uçak <69301617+OMNiA-cloud@users.noreply.github.com>
2022-02-12 20:30:34 +0300

    Add files via upload

---
+++ dvdrental_9.sql
@@ -0,0 +1,9 @@
+city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+=>SELECT city.city, country.country FROM CITY
+INNER JOIN COUNTRY ON city.city_id = country.country_id;
+
+customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+=>SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer INNER JOIN payment ON customer.customer_id = payment.customer_id;
+
+customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+=>SELECT customer.first_name, customer.last_name, rental.rental_id FROM customer INNER JOIN rental ON customer.customer_id = rental.customer_id;
